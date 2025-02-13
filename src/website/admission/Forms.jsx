b604f3477c012d6f8673a377593ad96134479390
rushikesh garad <garadrushikesh042@gmail.com>
2025-02-13 09:35:39 +0530

    admission sec complete

--- src/website/admission/Forms.jsx
+++ src/website/admission/Forms.jsx
@@ -7,13 +7,13 @@ export default function Forms()
     return(
         <>
             <div className="container-fluid">
-                <div className="row formouter">
+                <div className=" formouter">
                     <div className="box1">
-                        <img src="https://takalkarclasses.com/wp-content/uploads/2024/12/Takalkar-classes-admission.png" alt="" />
+                        <img src="https://takalkarclasses.com/wp-content/uploads/2024/12/Takalkar-classes-admission.png" className="admissionimg" alt="" />
                     </div>
-                    <div className="box2 ">
-                        <table border={'2px'}>
-                            <form action="abc.html" method="get">
+                    <div className="box1">
+                        <form action="abc.html" method="get">
+                            <table>
                                 <tr>
                                     <td>
                                         <input type="text" placeholder="Name" name="" id="" />
@@ -21,7 +21,7 @@ export default function Forms()
                                 </tr>
                                 <tr>
                                     <td>
-                                        <input type="email" placeholder="email" name="" id="" />
+                                        <input type="email" placeholder="email" name="" id="" required />
                                     </td>
                                 </tr>
                                 <tr>
@@ -29,8 +29,52 @@ export default function Forms()
                                         <input type="text" placeholder="Number" name="" id="" />
                                     </td>
                                 </tr>
-                            </form>
-                        </table>
+                                <tr>
+                                    <td>
+                                        <select name="" id="">
+                                            <option value="">--select course--</option>
+                                            <option value="">8th 9th ssc</option>
+                                            <option value="">10th ssc</option>
+                                            <option value="">8th 9th CBSE</option>
+                                            <option value="">10th CBSE</option>
+                                            <option value="">8th 9th ICSE</option>
+                                            <option value="">10th ICSE</option>
+                                            <option value="">11th 12th science </option>
+                                            <option value="">11th 12th science & JEE</option>
+                                            <option value="">11th 12th science & NEET</option>
+                                            <option value="">11th 12th science & CET</option>
+                                        </select>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <select name="" id="">
+                                            <option value="">-- select nearest center --</option>
+                                            <option value="">Aundh</option>
+                                            <option value="">Balaji Nagar</option>
+                                            <option value="">Baner</option>
+                                            <option value="">rahtani</option>
+                                            <option value="">pimpri</option>
+                                            <option value="">akurdi</option>
+                                            <option value="">katraj</option>
+                                            <option value="">chikhali</option>
+                                            <option value="">wakad</option>
+                                            <option value="">swarget</option>
+                                            <option value="">wakad</option>
+                                            <option value="">hadpsar</option>
+                                            <option value="">warje</option>
+                                            <option value="">narhe</option>
+                                            <option value="">kothrud</option>
+                                        </select>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <button className="btn btn-success">submit</button>
+                                    </td>
+                                </tr>
+                            </table>
+                        </form>
                     </div>
                 </div>
             </div>
